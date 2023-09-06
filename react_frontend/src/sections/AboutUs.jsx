6e3b09c80a692682a1bc053df420c3c81ef2acb4
Elizabeth-Panashe-Pfachi <testlolabtest@gmail.com>
2023-09-06 11:37:26 +0200

    Add frontend

---
+++ react_frontend/src/sections/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <section id="about-us" className="max-container max-sm:mt-12">
+            <section className="flex flex-col justify-start gap-5">
+                <h2>About Us</h2>
+                <p>We are a group of students who have experienced the challenges of working in team projects.
+                    <br />
+                    We know how difficult it can be to collaborate effectively and achieve the dream work.
+                    <br />
+                    That's why we created this review app, to help fellow students share their experiences and improve their collaboration skills.
+                    <br />
+                    Our app allows students to write reviews about their fellow students, providing valuable insights into their work ethic and collaboration style. This information can help students make informed decisions when choosing team members for future projects. No more awkward moments when you realize your team member is a procrastinator!
+
+                    Our project was inspired by our past experiences in team projects. We often wondered how we could improve our collaboration skills and wished we had an idea of what the work ethic of the other person was like in a team. We didn't want to have to search on multiple platforms to find this information, so we created this app to make it easy for students to share their experiences.
+                    <br />
+                    Join us and start sharing your experiences today! Let's make team projects great again!</p>
+            </section>
+        </section >
+    )
+}
+
+export default AboutUs;
