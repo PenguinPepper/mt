1dddd0aa15bf5c9ac0ec3be17d8acc3215a0edc3
Elizabeth-Panashe-Pfachi <testlolabtest@gmail.com>
2024-07-22 12:12:25 +0200

    Remove react frontend folder

--- react_frontend/src/constants/index.js
+++
@@ -1,7 +0,0 @@
-import header_logo from '../assets/images/header_logo.svg';
-
-export const navLinks = [
-    { href: "#home", label: "Home" },
-    { href: "#about-us", label: "About Us" },
-    { href: "#features", label: "Features" },
-];
