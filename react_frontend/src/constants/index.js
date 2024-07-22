0082c1bf05e40b0885e508b64d567ad429bb8515
Elizabeth-Panashe-Pfachi <testlolabtest@gmail.com>
2024-07-22 12:22:14 +0200

    Revert "Remove react frontend folder"
    This reverts commit 1dddd0aa15bf5c9ac0ec3be17d8acc3215a0edc3.

---
+++ react_frontend/src/constants/index.js
@@ -0,0 +1,7 @@
+import header_logo from '../assets/images/header_logo.svg';
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#features", label: "Features" },
+];
